8692a3f4fa383c05dfb57edafcf6244d9e4796a0
LOCO185 <jloco151@gmail.com>
2021-11-07 13:22:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let myStr = 'hi';
+console.log(myStr);
+
+let number =10;
+console.log(number)
+// let myObj = {
+//     key1:"value1"
+// };
+let myObject = new Object;
+console.log(myObject);
+
+let myBoolian = true;
+console.log(myBoolian);
+
+myNull = null;
+console.log(myNull);
+
+let myNan = NaN;
+console.log(myNan);
+
+let mySymbol = '@';
+console.log(mySymbol);
